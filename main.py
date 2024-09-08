387be3bed44ecf921a41cac46636506ad4ec788b
Ben Osborn <benosborntech@gmail.com>
2024-09-09 08:27:04 +1000

    updated outputs

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from index import generate_hash
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 SEED = 1234
+VEC_SIZE = 1536
+NUM_VEC = 100
 
-VEC_SIZE = 10
 DEPTH = 5
 
 def main():
     rand = random.Random(SEED)
 
-    vector = [2 * rand.random() - 1 for _ in range(VEC_SIZE)]
+    vectors = [[2 * rand.random() - 1 for _ in range(VEC_SIZE)] for _ in range(NUM_VEC)]
 
-    logger.info(f"vector: {vector}")
+    for vector in vectors:
+        logger.debug(f"vector: {vector}")
 
-    hsh = generate_hash(logger, vector, DEPTH)
+        hsh = generate_hash(logger, vector, DEPTH)
+
+        logger.info(f"out vector hash: {hsh}\n")
 
 if __name__ == "__main__":
     main()
